ffea4729ca6a545d14b3262771bca791ac58b6b6
expeditus303 <ricardotonietto@gmail.com>
2022-10-16 15:50:45 -0300

    Versão com código js reduzido

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let food1 = document.querySelector("div#food1");
-let food2 = document.querySelector("div#food2");
-let food3 = document.querySelector("div#food3");
-
-let beverage1 = document.querySelector("div#beverage1");
-let beverage2 = document.querySelector("div#beverage2");
-let beverage3 = document.querySelector("div#beverage3");
-
-let dessert1 = document.querySelector("div#dessert1");
-let dessert2 = document.querySelector("div#dessert2");
-let dessert3 = document.querySelector("div#dessert3");
-
 let food = "";
 let beverage = "";
 let dessert = "";
@@ -20,133 +8,76 @@ let priceDessert = 0;
 let button = document.querySelector(".button");
 let buttonActive = document.querySelector("#button");
 
-let checkOut = document.querySelector('.checkOut')
+let checkOut = document.querySelector(".checkOut");
 
-let checkOutFood = document.querySelector('#checkOutFood')
-let checkOutFoodPrice = document.querySelector('#checkOutFoodPrice')
+let checkOutFood = document.querySelector("#checkOutFood");
+let checkOutFoodPrice = document.querySelector("#checkOutFoodPrice");
 
-let checkOutBeverage = document.querySelector('#checkOutBeverage')
-let checkOutBeveragePrice = document.querySelector('#checkOutBeveragePrice')
+let checkOutBeverage = document.querySelector("#checkOutBeverage");
+let checkOutBeveragePrice = document.querySelector("#checkOutBeveragePrice");
 
-let checkOutDessert = document.querySelector('#checkOutDessert')
-let checkOutDessertPrice = document.querySelector('#checkOutDessertPrice')
+let checkOutDessert = document.querySelector("#checkOutDessert");
+let checkOutDessertPrice = document.querySelector("#checkOutDessertPrice");
 
-let checkOutTotalPrice = document.querySelector('#checkOutTotalPrice')
+let checkOutTotalPrice = document.querySelector("#checkOutTotalPrice");
 
 let totalValue = 0;
 let totalValueFloat = 0;
 
-function selectFood1() {
-  food1.classList.add("selected");
-  food2.classList.remove("selected");
-  food3.classList.remove("selected");
+function selectFood(selector) {
+  let lastSelected = document.querySelector(".foods .selected");
+  if (lastSelected !== null) {
+    lastSelected.classList.remove("selected");
+  }
+  selector.classList.add("selected");
 
-  food = "Frango Yin Yang";
-  priceFood = 14.90;
+  food = selector.querySelector(".food_name").innerHTML;
+
+  let price = selector
+    .querySelector(".food_price")
+    .innerHTML.replace("R$ ", "")
+    .replace(",", ".");
+  priceFood = Number(price);
 
   if (food !== "" && beverage !== "" && dessert !== "") {
     buttonActive.classList.remove("hide");
   }
 }
 
-function selectFood2() {
-  food1.classList.remove("selected");
-  food2.classList.add("selected");
-  food3.classList.remove("selected");
+function selectBeverage(selector) {
+  let lastSelected = document.querySelector(".beverages .selected");
+  if (lastSelected !== null) {
+    lastSelected.classList.remove("selected");
+  }
+  selector.classList.add("selected");
 
-  food = "Pizza Romana";
-  priceFood = 25.90;
+  beverage = selector.querySelector(".food_name").innerHTML;
+
+  let price = selector
+    .querySelector(".food_price")
+    .innerHTML.replace("R$ ", "")
+    .replace(",", ".");
+  priceBeverage = Number(price);
 
   if (food !== "" && beverage !== "" && dessert !== "") {
     buttonActive.classList.remove("hide");
   }
 }
 
-function selectFood3() {
-  food1.classList.remove("selected");
-  food2.classList.remove("selected");
-  food3.classList.add("selected");
-
-  food = "Burguer NY";
-  priceFood = 17.90;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
+function selectDessert(selector) {
+  let lastSelected = document.querySelector(".desserts .selected");
+  if (lastSelected !== null) {
+    lastSelected.classList.remove("selected");
   }
-}
+  selector.classList.add("selected");
 
-function selectBeverage1() {
-  beverage1.classList.add("selected");
-  beverage2.classList.remove("selected");
-  beverage3.classList.remove("selected");
+  dessert = selector.querySelector(".food_name").innerHTML;
 
-  beverage = "Coquinha gelada";
-  priceBeverage = 4.90;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
-  }
-}
-
-function selectBeverage2() {
-  beverage1.classList.remove("selected");
-  beverage2.classList.add("selected");
-  beverage3.classList.remove("selected");
-
-  beverage = "Suco de laranjinha";
-  priceBeverage = 8.00;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
-  }
-}
-
-function selectBeverage3() {
-  beverage1.classList.remove("selected");
-  beverage2.classList.remove("selected");
-  beverage3.classList.add("selected");
-
-  beverage = "Cerveja trincando";
-  priceBeverage = 6.90;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
-  }
-}
-
-function selectDessert1() {
-  dessert1.classList.add("selected");
-  dessert2.classList.remove("selected");
-  dessert3.classList.remove("selected");
-
-  dessert = "Pudim";
-  priceDessert = 7.90;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
-  }
-}
-
-function selectDessert2() {
-  dessert1.classList.remove("selected");
-  dessert2.classList.add("selected");
-  dessert3.classList.remove("selected");
-
-  dessert = "BOMBA! (de chocolate)";
-  priceDessert = 5.00;
-
-  if (food !== "" && beverage !== "" && dessert !== "") {
-    buttonActive.classList.remove("hide");
-  }
-}
-
-function selectDessert3() {
-  dessert1.classList.remove("selected");
-  dessert2.classList.remove("selected");
-  dessert3.classList.add("selected");
-
-  dessert = "Picolé de morango";
-  priceDessert = 3.99;
+  let price = selector
+    .querySelector(".food_price")
+    .innerHTML.replace("R$ ", "")
+    .replace(",", ".");
+  priceDessert = Number(price);
 
   if (food !== "" && beverage !== "" && dessert !== "") {
     buttonActive.classList.remove("hide");
@@ -154,41 +85,44 @@ function selectDessert3() {
 }
 
 function makeOrder() {
-  totalValue = priceFood + priceBeverage + priceDessert;
-  totalValueFloat = totalValue.toFixed(2)
+  totalValueFloat = (priceFood + priceBeverage + priceDessert).toFixed(2);
 
-  checkOutFood.innerHTML = (food)
-  checkOutFoodPrice.innerHTML = (`R$ ${priceFood.toFixed(2)}`)
+  checkOutFood.innerHTML = food;
+  checkOutFoodPrice.innerHTML = `R$ ${priceFood.toFixed(2).replace(".", ",")}`;
 
-  checkOutBeverage.innerHTML = (beverage)
-  checkOutBeveragePrice.innerHTML = (`R$ ${priceBeverage.toFixed(2)}`)
+  checkOutBeverage.innerHTML = beverage;
+  checkOutBeveragePrice.innerHTML = `R$ ${priceBeverage
+    .toFixed(2)
+    .replace(".", ",")}`;
 
-  checkOutDessert.innerHTML = (dessert)
-  checkOutDessertPrice.innerHTML = (`R$ ${priceDessert.toFixed(2)}`)
+  checkOutDessert.innerHTML = dessert;
+  checkOutDessertPrice.innerHTML = `R$ ${priceDessert
+    .toFixed(2)
+    .replace(".", ",")}`;
 
-  checkOutTotalPrice.innerHTML = totalValueFloat
+  checkOutTotalPrice.innerHTML = `R$ ${totalValueFloat.replace(".", ",")}`;
 
-  checkOut.classList.remove('hide')
+  checkOut.classList.remove("hide");
 }
 
 function checkOutOk() {
-  let name = window.prompt('Por favor, digite o seu nome:')
-  let address = window.prompt('Por favor, digite o endereço de entrega:')
+  let name = window.prompt("Por favor, digite o seu nome:");
+  let address = window.prompt("Por favor, digite o endereço de entrega:");
 
-  let message = (`Olá, gostaria de fazer o pedido:
+  let message = `Olá, gostaria de fazer o pedido:
   - Prato: ${food}
   - Bebida: ${beverage}
   - Sobremesa: ${dessert}
   Total: R$ ${totalValueFloat}
   
   Nome: ${name}
-  Endereço: ${address}`)
+  Endereço: ${address}`;
 
-  let encodeMessage = encodeURIComponent(message)
+  let encodeMessage = encodeURIComponent(message);
 
   window.open("https://wa.me/5548996581772?text=" + encodeMessage);
 }
 
 function checkOutCancel() {
-  checkOut.classList.add('hide')
+  checkOut.classList.add("hide");
 }
